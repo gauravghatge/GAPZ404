7b945e1f3969c13500586690d62a8d84062fb5d2
Gaurav Ramesh Ghatge <55950099+gauravghatge@users.noreply.github.com>
2020-12-30 19:36:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config();
-const URL = "mongodb+srv://guru404:j6KA2DmpNBScPIip@evento-webapp.hixrh.mongodb.net/evento?retryWrites=true&w=majority";
+const URL = process.env.DB_URL;
 
 const connectDB = async () => {
   try {
